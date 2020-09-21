c474167742c9ec288f486a4d9b701b66daf7c71f
carryegaulin31 <71082001+carryegaulin31@users.noreply.github.com>
2020-09-20 23:29:02 -0400

    answer-part-one

---
+++ script.js
@@ -0,0 +1,8 @@
+function sum() {
+				
+    var x = Number(document.getElementById("number1").value);
+    var y = Number(document.getElementById("number2").value);
+    document.getElementById("answer").value = x + y;
+    alert(x + y)
+}
+  
