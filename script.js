85b6add8957e69108ef6a549255aa3415985dce1
carryegaulin31 <71082001+carryegaulin31@users.noreply.github.com>
2020-09-23 23:16:43 -0400

    css added

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function sum() {
-				
-    var x = Number(document.getElementById("number1").value);
+	var x = Number(document.getElementById("number1").value);
     var y = Number(document.getElementById("number2").value);
-    document.getElementById("answer").value = x + y;
+    document.getElementById("answer").value = x + y
     alert(x + y)
 }
-  
+function subtract() {
+	var x = Number(document.getElementById("number1").value);
+    var y = Number(document.getElementById("number2").value);
+    document.getElementById("answer").value = x - y
+    alert(x - y)
+}
+function multiply() {
+	var x = Number(document.getElementById("number1").value);
+    var y = Number(document.getElementById("number2").value);
+    document.getElementById("answer").value = x * y
+    alert(x * y)
+}
+function divide() {
+	var x = Number(document.getElementById("number1").value);
+    var y = Number(document.getElementById("number2").value);
+    document.getElementById("answer").value = x / y
+    alert(x / y)
+}
