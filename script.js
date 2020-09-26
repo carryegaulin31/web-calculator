c921c8a105677226243e2d723a1104ca37f8b547
carryegaulin31 <71082001+carryegaulin31@users.noreply.github.com>
2020-09-25 21:17:36 -0400

    last time

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-function sum() {
-	var x = Number(document.getElementById("number1").value);
-    var y = Number(document.getElementById("number2").value);
-    document.getElementById("answer").value = x + y
-    alert(x + y)
+function calculate() {
+  var x = Number(document.getElementById("number1").value);
+  var y = Number(document.getElementById("number2").value);
+
+  var operator = document.getElementById("operation").value;
+  var answer = document.getElementById("answer")
+  answer.value = getAnswer(operator, x, y)
+  //instead of previous 2 lines could do this document.getElementById("answer").value = getAnswer(operator, x, y)
 }
-function subtract() {
-	var x = Number(document.getElementById("number1").value);
-    var y = Number(document.getElementById("number2").value);
-    document.getElementById("answer").value = x - y
-    alert(x - y)
-}
-function multiply() {
-	var x = Number(document.getElementById("number1").value);
-    var y = Number(document.getElementById("number2").value);
-    document.getElementById("answer").value = x * y
-    alert(x * y)
-}
-function divide() {
-	var x = Number(document.getElementById("number1").value);
-    var y = Number(document.getElementById("number2").value);
-    document.getElementById("answer").value = x / y
-    alert(x / y)
-}
+
+function getAnswer(operator, x, y) {
+  if (operator === "+") {
+    return x + y
+  } else if (operator === "-") {
+    return x - y
+  } else if (operator === "*") {
+    return x * y
+  } else if (operator === "/") {
+    return x / y
+  }
