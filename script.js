0278dd683c00a7209f0799ad0b0067fa591bbe60
carrye <carryegaulin033178@gmail.com>
2020-10-02 12:43:17 -0400

    fixed

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function calculate() {
   var x = Number(document.getElementById("number1").value);
   var y = Number(document.getElementById("number2").value);
-
   var operator = document.getElementById("operation").value;
-  var answer = document.getElementById("answer")
-  answer.value = getAnswer(operator, x, y)
-  //instead of previous 2 lines could do this document.getElementById("answer").value = getAnswer(operator, x, y)
+
+  document.getElementById("answer").value = getAnswer(operator, x, y)
 }
 
 function getAnswer(operator, x, y) {
@@ -18,3 +16,4 @@ function getAnswer(operator, x, y) {
   } else if (operator === "/") {
     return x / y
   }
+} 
